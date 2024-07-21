7d1821f31b0cecdc620fbd05110a98b51d132a4d
Amit Vishwakarma <vishamit8827@gmail.com>
2024-07-21 15:10:07 +0530

    c lang

---
+++ Chapter-1/01_variable.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a; //Deceleration of the Variable
+    a = 7; //Initilaziation of the Varibale
+    
+    printf("Your valriable is %d", a);
+  
+    return 0;
+}
---
+++ Chapter-1/02_dataTypes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int a = 1; //Int - it take 4 bytes
+    float b = 4.2; //float Decimal Numbers - take 4 bytes
+    char c = 'a'; // written in single quotes - take 1 bytes
+
+    printf("Data Types %c", c);
+
+}
